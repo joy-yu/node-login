d8579e580ee41b2a7a6aaa29e7e9b1cb03fd15cd
joyu <775154773@qq.com>
2017-02-28 17:32:51 +0800

    decouple register logic

--- routes/index.js
+++ routes/index.js
@@ -28,72 +28,8 @@ router.get('/logout', function(req, res) {
   });
 });
 
-/* 注册成功页面 */
-router.get('/regsuccess', function(req, res) {
- if(req.session.user){
-    res.render('regsuccess', {
-      title: '注册成功！'
-    });
-  } else{
-    res.redirect('/'); 
-  }
-});
 
 
-
-/* 注册页 */  
-router.route("/register")
-.get(function(req, res) {
-  res.render("register", {
-    title: '注册页~'
-  });
-})
-
-.post(function(req, res) {
-
-  var uid = req.body.userid;
-  var upwd = req.body.password;
-  var uhash;
-
-  user.findOne({
-    userid: uid
-  }, function(err, doc) {
-    if (err) {
-      res.sendStatus(500);
-      console.log(err);
-    } else if (doc) {
-      res.json({regState: 0});
-      console.log('用户名已存在！');
-    } else {
-
-      //加密
-      crypto.randomBytes(31, function(err, salt) {
-        if (err) {
-          throw err;
-        }
-
-        uhash = crypto.pbkdf2Sync(upwd, salt.toString('hex'), 10000, 256, 'sha512');
-        uhash = uhash.toString('hex');
-
-        // 创建一组user对象置入model
-        user.create({
-          userid: uid,
-          password: uhash,
-          salt: salt.toString('hex')
-        }, function(err, doc) {
-          if (err) {
-            res.sendStatus(500);
-            console.log(err);
-          } else {
-            req.session.user = doc;
-            res.json({regState: 1});
-            console.log('注册成功！');
-          }
-        });
-      });
-    }
-  });
-});
 
 
 /*登陆验证*/
